bcd33f76798f48400795e771f3f9128ab3f07242
xoten herself <ana.schotten@estudante.ifms.edu.br>
2024-08-16 17:12:54 -0400

    .

--- src/Produto/index.jsx
+++ src/Produto/index.jsx
@@ -1,62 +1,14 @@
-import { useState } from "react";
 import Header from "../components/Header";
+import Produtos from "../components/Produtos";
+import Footer from "../components/Footer";
 
 export default function Produto() {
-
-    const [listaProdutos, setProdutos] = useState([
-        { id: 1, nome: '', preço: 19.99, imagem: '' },
-        { id: 2, nome: '', preço: 19.99, imagem: '' }
-    ]);
-
-    const [listaPedidos, setListaPedidos] = useState([]);
-
-    const adicionarItemPedidos = (objeto) => {
-        setListaPedidos([...listaPedidos, objeto])
-    }
-
-    const removerPedido = (id) => {
-        let remover = false;
-        let listaAux = listaPedidos.filter((produto) => {
-            if (remover == false) {
-                if (produto.id !== id) {
-                    return produto
-                } else {
-                    remover = true;
-                    return null
-                }
-            } else {
-                return produto
-            }
-        });
-        setListaPedidos(listaAux);
-    }
-
     return (
-        <div>
+        <>
             <Header />
-            <h1>Produtows</h1>
-            <>
-                {
-                    listaProdutos.map((produto) =>
-                        <div key={produto.id}>
-                            <img src={produto.imagem} />
-                            <p>{produto.nome}</p>
-                            <p>R$</p><p>{produto.preço}</p>
-                            <button onClick={() => adicionarItemPedidos(produto)}>Adicionar ao Carrinho!</button>
-                        </div>
-                    )
-                }
-                {
-                    listaPedidos.map((produto) =>
-                        <div key={produto.id} id="Prod">
-                            <img src={produto.imagem} />
-                            <p>{produto.nome}</p>
-                            <p>R$</p><p>{produto.preço}</p>
-                            <button onClick={() => removerPedido(produto.id)}>Remover</button>
-                        </div>
-                    )
-                }
-            </>
-        </div>
+            <h1>Produtos</h1>
+            <Produtos />
+            <Footer />
+        </>
     );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p>Desenvolvido por Ana Paula S. Schotten - INFO 3B</p>
+        </footer>
+    );
+}  
