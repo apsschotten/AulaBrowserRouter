6837c99976275dea75f76846ed517bec09998520
xoten herself <ana.schotten@estudante.ifms.edu.br>
2024-08-16 16:53:32 -0400

    ;

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Produto from './Produto'
 import Oferta from './Oferta'
+import App from './App'
+import "./globals.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/produto" element={<Produto />} />
-        <Route path="/oferta" element={<Oferta />} />
+      <Route element={<App/>} path='/'/>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Produto" element={<Produto />} />
+        <Route path="/Oferta" element={<Oferta />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
