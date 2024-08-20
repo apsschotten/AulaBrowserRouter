e4493e0f8ba4ab3b70ff5a09aea9d54469085db7
apsschotten <ana.schotten@estudante.ifms.edu.br>
2024-08-20 16:05:29 -0300

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,26 @@ import '../globals.css'
 export default function Header() {
     return (
         <header>
-            <img src="src/images/Logo.png" class="hImage"/>
-            <ul>
-                <Link to="/home">
-                    <li>Home</li>
-                </Link>
-                <Link to="/produto">
-                    <li>Produtos</li>
-                </Link>
-                <Link to="/oferta">
-                    <li>Ofertas</li>
-                </Link>
-            </ul>
+            <div class="iTop">
+            <img src="src/images/Logo.png" class="hImage" />
+            </div>
+            <div class="bList">
+                <button class="bTop">
+                    <Link to="/home">
+                        <li>Home</li>
+                    </Link>
+                </button>
+                <button class="bTop">
+                    <Link to="/produto">
+                        <li>Produtos</li>
+                    </Link>
+                </button>
+                <button class="bTop">
+                    <Link to="/oferta">
+                        <li>Ofertas</li>
+                    </Link>
+                </button>
+            </div>
         </header>
     );
 }  
